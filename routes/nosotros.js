9fcd42d5771ddc331e208141b0c4e1c0bf9d88c4
MariAyup <125048022+MariAyup@users.noreply.github.com>
2023-05-01 12:30:07 -0300

    Add files via upload

--- routes/nosotros.js
+++ routes/nosotros.js
@@ -4,8 +4,11 @@ var db = require("../conexion/conexion");
 
 /*GET section. (nosotros.ejs)*/
 router.get('/', function(req, res, next) {
+
     res.render('nosotros', { title: 'Nosotros somos' });
-});
+  });
+
+  
 
 module.exports = router;
 
