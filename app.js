4f7c411b43acf0d213b50cc9f39e5fa3d85318ee
MariAyup <125048022+MariAyup@users.noreply.github.com>
2023-07-10 10:55:34 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var nosotrosRouter = require('./routes/nosotros');
 var problemasRouter = require('./routes/problemas');
+var cargaproblemasRouter = require('./routes/cargaproblemas');
+var testsRouter = require('./routes/tests');
 
 var app = express();
 
@@ -25,6 +27,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/nosotros', nosotrosRouter);
 app.use('/problemas', problemasRouter);
+app.use('/cargaproblemas', cargaproblemasRouter);
+app.use('/tests', testsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
