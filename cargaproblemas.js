a7166380124488a0e6a5b78790918b1bb0030a4e
MariAyup <125048022+MariAyup@users.noreply.github.com>
2023-08-09 21:35:26 -0300

    Add files via upload

--- cargaproblemas.js
+++ cargaproblemas.js
@@ -1,30 +1,35 @@
 var express = require('express');
 var router = express.Router();
 var db = require("../conexion/conexion");
-//var app = express();
-router.use(express.urlencoded({ extended: true }));
-router.use(express.json());
+const problemasController=require("../controllers/problemasController");
+var problema=require("../model/problema");
+var app = express();
+//router.use(express.urlencoded({ extended: true }));
+//router.use(express.json());
 
-/*GET section. (cargaproblemas.ejs)*/
+/*GET section. (cargaproblemas.ejs)
+router.get('/', function(req, res, next) {
+  
+    res.render('cargaproblemas', { title: 'Carga de Problemas' });
+  }); */
+
 router.get('/', function(req, res, next) {
 
-    res.render('cargaproblemas', { title: 'Carga de Problemas' });
-  });
+    db.query("SELECT * FROM problemas" , function(err, resultados){
+    
+        res.render('cargaproblemas', { title: 'Problemas', Problemas:resultados });
+        
+    
+    })
+    });
+//router.get('/', problemasController.index);
+//router.get('/crear', problemasController.crear);
 
 // Ruta para crear un nuevo registro
-router.post('/crear', function (req, res)  {
-  const { titulo, descripcion, oraculo, test, codigo } = req.body; // Obtén los datos enviados desde el formulario
+//router.post('/', problemasController.guardar);  
+  
 
-  // Realiza la consulta SQL para insertar los datos en la base de datos
-  db.query("INSERT * INTO problemas (Titulo_problema, Descripcion, Oraculo, Test, Codigo) VALUES (?, ?, ?, ?, ?)", [titulo, descripcion, oraculo, test, codigo], function(error, results) {
-    if (error) {
-      console.error('Error al crear el registro: ', error);
-    } else {
-      console.log('Registro creado exitosamente.');
-      res.redirect('/'); // Redirecciona a la página principal después de crear el registro
-    }
-  });
-});
+
 
   
 
